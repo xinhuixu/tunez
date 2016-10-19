4a3091c7683276df2319ea89ac63ae1808482061
Xinhui <x.xu.1102@gmail.com>
2016-10-19 14:53:51 -0400

    minor fix

--- tunez.c
+++ tunez.c
@@ -30,7 +30,7 @@ song_node * free_list(song_node *n) {
 
 //find and return a pointer to an individual song based on song name
 
-struct song_node * search_list(struct song_node * n, char * name){
+ song_node * search_list( song_node * n, char * name){
   while (n){
     if (! strcmp(name, n->name))
       n = n->next;
@@ -40,22 +40,22 @@ struct song_node * search_list(struct song_node * n, char * name){
   return n;
 }
 
-struct song_node * print_song(struct song_node *n){
+ song_node * print_song( song_node *n){
   printf("song: %s\n", n->name);
 }
 
 int main(){
-  struct song_node *a = (struct song_node *)malloc(sizeof(struct song_node));
+   song_node *a = ( song_node *)malloc(sizeof( song_node));
   strcpy(a->name, "chasing ghosts");
   strcpy(a->artist, "atc");
-  struct song_node *b = (struct song_node *)malloc(sizeof(struct song_node));
+   song_node *b = ( song_node *)malloc(sizeof( song_node));
   strcpy(b->name, "runaway");
   strcpy(b->artist, "btc");
-  struct song_node *c = (struct song_node *)malloc(sizeof(struct song_node));
+   song_node *c = ( song_node *)malloc(sizeof( song_node));
   strcpy(c->name, "hello");
   strcpy(c->artist, "cdele");
-  struct song_node *d = (struct song_node *)malloc(sizeof(struct song_node));
-  struct song_node *addon = (struct song_node *)malloc(sizeof(struct song_node));
+   song_node *d = ( song_node *)malloc(sizeof( song_node));
+   song_node *addon = ( song_node *)malloc(sizeof( song_node));
 
   a->next = b;
   b->next = c;
